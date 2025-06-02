2d5bc26385dd40f839782233243a6e690bae60d2
tvjudge <tvjudge@learnubuntu.org>
2025-06-02 11:44:16 -0400

    Initial UI Code
    copied initial UI code into repo usong initual-ui-development branch

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "custom-navbar">
+                    
+                            Sample CRUD Application
+                    
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
